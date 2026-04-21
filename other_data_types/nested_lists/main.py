ff903936f3eff3a199e88ba6aad352f335afcb37
ecasmo-coder <>
2026-04-21 00:59:29 +0000

    Nested Lists

---
+++ other_data_types/nested_lists/main.py
@@ -0,0 +1,6 @@
+vegetables = ["tomatoes", "potatoes", "onions"]
+vegetables.remove("onions")
+vegetables.append("carrots")
+vegetables.append("cucumbers")
+
+print("Updated Vegetable Inventory:",vegetables)
