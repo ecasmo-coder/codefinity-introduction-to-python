aae3624c9ea3e4432e4c7e48ff33cd1422268ef0
ecasmo-coder <>
2026-04-21 01:04:19 +0000

    String Manipulation

---
+++ variables_and_types/string_manipulation/main.py
@@ -0,0 +1,8 @@
+grocery_items = "milk cheese bread apples oranges chicken"
+dairy1 = grocery_items [0:3]
+dairy2 = grocery_items [5:10]
+bakery1 = grocery_items [12:16]
+
+#using concatenation +
+
+print(f"We have dairy and bakery items: {dairy1} + {dairy2} + {bakery1} in aisle 5")
